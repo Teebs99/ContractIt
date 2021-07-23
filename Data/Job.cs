b503b44cd91355e4eb78e583f40cf95387263c7e
Stephen Ives <teebs99@gmail.com>
2021-07-23 10:52:17 -0400

    Created required Job models and finished job services

--- Data/Job.cs
+++ Data/Job.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data
 {
-    class Job
+    public class Job
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        //[ForeignKey(nameof(Contractor))]
+        //public int ContractorId { get; set; }
+        //public Contractor Contractor { get; set; }
+        //[ForeignKey(nameof(Category))]
+        //public int CategoryId { get; set; }
+        //public Category Category { get; set; }
     }
 }
---
+++ Services/JobService.cs
@@ -0,0 +1,72 @@
+﻿using ContractIt.Models;
+using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    class JobService
+    {
+        private Guid _userId;
+        public JobService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool AddJob(JobCreate model)
+        {
+            var entity = new Job() { Title = model.Title, Description = model.Description, Address = model.Address, PhoneNumber = model.PhoneNumber };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Jobs.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<JobListItem> GetJobs()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Jobs.Where(q => q.AuthorId == _userId).Select(q => new JobListItem() { Title = q.Title, Description = q.Description });
+                return query.ToArray();
+            }
+        }
+        public JobDetail GetJob(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Jobs.Where(e => e.AuthorId == _userId).Single(e => e.Id == id);
+                return new JobDetail()
+                {
+                    Title = entity.Title,
+                    Description = entity.Description,
+                    PhoneNumber = entity.PhoneNumber,
+                    Address = entity.Address
+                };
+            }
+        }
+        public bool UpdateJob(int id, JobEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Jobs.Single(e => e.Id == id && e.AuthorId == _userId);
+                entity.Title = model.Title;
+                entity.Description = model.Description;
+                entity.PhoneNumber = model.PhoneNumber;
+                entity.Address = model.Address;
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteJob(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Jobs.Single(e => e.Id == id && e.AuthorId == _userId);
+                ctx.Jobs.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
