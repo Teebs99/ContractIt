6cb799e4e2fea750700c73c9fe0dd968681dc64c
Stephen Ives <teebs99@gmail.com>
2021-07-23 11:17:50 -0400

    Added Controller with basic endpoints, updated service class to ensure correct output

--- Models/JobListItem.cs
+++ Models/JobListItem.cs
@@ -8,6 +8,7 @@ namespace Models
 {
     public class JobListItem
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
     }
