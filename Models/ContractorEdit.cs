76e6e650293c83d55e2ec6f77383527679d35eea
Melaniezrr <85641958+Melaniezrr@users.noreply.github.com>
2021-07-26 09:33:58 -0400

    Contractor Models and Service

--- Models/ContractorEdit.cs
+++ Models/ContractorEdit.cs
@@ -1,24 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Data
+namespace Models
 {
-    class Contractor
+    class ContractorEdit
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
-
-        [Required]
         public string Description { get; set; }
-
-        [Required]
         public string PhoneNumber { get; set; }
     }
 }
